0fad1b1f83de2e4be931b55139ae8e61b0afcd70
Keane <KeaneCarotenuto@gmail.com>
2022-05-31 15:19:59 +1200

    Installing mongoose, faker, bodyparser

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 const { restart } = require('nodemon');
-const sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
 const { exec } = require('child_process');
 
@@ -18,7 +17,7 @@ exec("start cmd /c lt --port 4000")
 
 //session middleware
 app.use(sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    secret: "thisismysecrctekeyfhnorgfgrfrty84fwir767",
     saveUninitialized:true,
     cookie: { maxAge: oneDay },
     resave: false
@@ -69,44 +68,44 @@ app.get('/profile',(req,res) => {
 });
 
 app.post('/dosignin',(req,res) => {    
-    let db = new sqlite3.Database('./db/database.db');
-    db.get("SELECT password FROM logins WHERE username = ?", [req.body.username], (err,row) => {
-        if(err) {
-            console.log(err.message);
-        }
-        if(row){
-            if(row.password === req.body.password){
-                session = req.session;
-                session.userid = req.body.username;
-                console.log(session);
-                res.redirect('/');
-            }else{
-                res.send("Invalid Password <a href=\'/\'>click to go back</a>");
-            }
-        }else{
-            res.send("Invalid Username <a href=\'/\'>click to go back</a>");
-        }
-    });
+    // let db = new sqlite3.Database('./db/database.db');
+    // db.get("SELECT password FROM logins WHERE username = ?", [req.body.username], (err,row) => {
+    //     if(err) {
+    //         console.log(err.message);
+    //     }
+    //     if(row){
+    //         if(row.password === req.body.password){
+    //             session = req.session;
+    //             session.userid = req.body.username;
+    //             console.log(session);
+    //             res.redirect('/');
+    //         }else{
+    //             res.send("Invalid Password <a href=\'/\'>click to go back</a>");
+    //         }
+    //     }else{
+    //         res.send("Invalid Username <a href=\'/\'>click to go back</a>");
+    //     }
+    // });
 
-    db.close();
+    // db.close();
 })
 
 app.post('/dosignup',(req,res) => {
     //+check that both passwords match
-    if(req.body.password === req.body.confirmpassword){
-        let db = new sqlite3.Database('./db/database.db');
-        db.run("INSERT INTO logins (username,password) VALUES (?,?)", [req.body.username,req.body.password], (err) => {
-            if(err) {
-                console.log(err.message);
-            }
-            console.log("New user created");
-        });
-        db.close();
-        res.redirect('/');
-    }
-    else{
-        res.send("Passwords do not match <a href=\'/newuser\'>click to go back</a>");
-    }
+    // if(req.body.password === req.body.confirmpassword){
+    //     let db = new sqlite3.Database('./db/database.db');
+    //     db.run("INSERT INTO logins (username,password) VALUES (?,?)", [req.body.username,req.body.password], (err) => {
+    //         if(err) {
+    //             console.log(err.message);
+    //         }
+    //         console.log("New user created");
+    //     });
+    //     db.close();
+    //     res.redirect('/');
+    // }
+    // else{
+    //     res.send("Passwords do not match <a href=\'/newuser\'>click to go back</a>");
+    // }
 })
 
 app.get('/logout',(req,res) => {
